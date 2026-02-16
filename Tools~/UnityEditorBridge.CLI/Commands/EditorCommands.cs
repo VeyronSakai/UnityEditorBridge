e57b311fa6fc8c7c7d3886a7403d29b713579a67
Yuki Sakai <yuukisakai123@gmail.com>
2026-02-16 23:42:09 +0900

    Update EditorCommands.cs

--- Tools~/UnityEditorBridge.CLI/Commands/EditorCommands.cs
+++ Tools~/UnityEditorBridge.CLI/Commands/EditorCommands.cs
@@ -1,9 +1,12 @@
+using System.Diagnostics.CodeAnalysis;
+
 namespace UnityEditorBridge.CLI.Commands;
 
+[SuppressMessage("Performance", "CA1822:Mark members as static")]
 public class EditorCommands
 {
     /// <summary>Check server connectivity.</summary>
-    public static async Task Ping()
+    public async Task Ping()
     {
         await BridgeClient.GetAsync("/ping");
     }
